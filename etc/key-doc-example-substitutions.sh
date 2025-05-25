c6bb70849b97175e4c98e4a6939e55689d889d10
phynix <phynix@phoggy.me>
2025-05-25 10:50:36 -0700

    In progress key doc updates. Broken at the moment.

--- etc/key-doc-example-substitutions.sh
+++ etc/key-doc-example-substitutions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 author='Bob Barker'
-closing='Happy trails(!)'
+closing='Thank You!'
 contacts=(
     'Billy Bob. 555-555-1212, billy_bob@gmail.com, 14 Main Street, Small Town, AR, 95466'
     'Aunt Martha. 555-555-1212, martha@aol.com, 14 Main Street, Small Town, AR, 95466'
