9b37cb1ff498c76dfa5e72b44dfbf3dea95eba65
phynix <phynix@phoggy.me>
2025-06-19 19:41:36 -0700

    PDF generation improvements.

--- etc/key-info-template.sh
+++ etc/key-info-template.sh
@@ -4,23 +4,33 @@
 # customize the generated PDF. The PDF contains a backup of the keys in
 # paper form (aka "paper key") along with usage instructions.
 
-# The first page is in the form of a letter, where you'll have a closing line
-# followed by your name as 'signature'.
+# The first page is in the form of a letter, where you'll have a greeting
+# and a closing line followed by your name as 'signature'.
 
+greeting="Greetings,"
 closing="I hope you never need this!"
 author="Bob Barker"
 
 # In the event that you are unavailable or incapacitated when encrypted file
 # content is needed, you must provide contact info for at least one person
-# with appropriate computer skills (MacOS command line tools) to help.
+# with appropriate computer skills (i.e. MacOS command line tools) to help.
 #
-# This is POTENTIALLY CRITICAL information, so please give it the attention
+# This is potentially CRITICAL information, so please give it the attention
 # it deserves!
 
 contacts=(
     "Billy Bob: +1 555-555-1212, @billy_bob1725, billy_bob@gmail.com. 14 Main Street, Small Town, AR 95466 USA"
     "Aunt Martha: +44 791 112 4456, @aunt_martha1, martha@aol.com. 221B Baker Street, London, NW1 6XE UNITED KINGDOM"
 )
+
+# Include any additional information here. If you use the --no-passphrase
+# option, you SHOULD include hints or instructions here to inform the reader
+# how to access the passphrase and you cannot leave this value empty.
+#
+# Each entry in the array will be printed as a separate line. Long lines
+# can be split by appending '\' without closing the quote.
+
+notes=""
 
 # Please change the following to "yes" when you have completed filling out
 # the information above.
